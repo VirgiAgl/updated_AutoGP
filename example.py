54ebb8d33cb72c0b48df2bb76eae5b8dd328f597
Virginia Aglietti <aglietti@toucanet.stats.ox.ac.uk>
2017-04-20 16:52:37 +0100

    text new lik in example

--- example.py
+++ example.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 
 # Generate synthetic data.
 N_all = 200
-N =  5
+N =  50
 inputs = 5 * np.linspace(0, 1, num=N_all)[:, np.newaxis]
 outputs = np.sin(inputs)
 
@@ -19,8 +19,8 @@ xtest = inputs[idx[N:]]
 ytest = outputs[idx[N:]]
 
 # Initialize the Gaussian process.
-likelihood = autogp.likelihoods.LGCP()
-kernel = [autogp.kernels.Polynomial(1)]
+likelihood = autogp.likelihoods.Logistic()
+kernel = [autogp.kernels.RadialBasis(1)]
 inducing_inputs = xtrain
 model = autogp.GaussianProcess(likelihood, kernel, inducing_inputs)
 
@@ -29,10 +29,10 @@ optimizer = tf.train.RMSPropOptimizer(0.005)
 model.fit(data, optimizer, loo_steps=0, var_steps=50, epochs=100)
 
 # Predict new inputs.
-ypred, _ = model.predict(xtest)
-plt.plot(xtrain, ytrain, '.', mew=2)
-plt.plot(xtest, ytest, 'o', mew=2)
-plt.plot(xtest, ypred, 'x', mew=2)
+ypred, _ = model.predict(xtest) #V_the command predict gives back the predicted mean and the predicted variance corresponding to the xtest
+plt.plot(xtrain, ytrain, '.', mew=2) #V_plot the points used to train the model
+plt.plot(xtest, ytest, 'o', mew=2) #V_plot the points used to trest the model
+plt.plot(xtest, ypred, 'x', mew=2) #V_plot the tested x with the predicted y
 plt.show()
 
 
